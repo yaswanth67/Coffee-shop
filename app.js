0dd43650f0cf6c78e4d1256b3751b113547e4769
John-Paul Ore <579990+jpwco@users.noreply.github.com>
2023-10-24 10:37:33 -0400

    adding tests

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static('public')); 
+module.exports = app
 
 // Endpoint to fetch available coffees
 app.get('/coffees', (req, res) => {
